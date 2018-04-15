c4c16df1e380d4b409e169d04d56bca5c0307a76
r-jain <rohitjain.pr@gmail.com>
2018-04-16 02:47:26 +0530

    Recognizer Implementation

--- my_recognizer.py
+++ my_recognizer.py
@@ -20,6 +20,34 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    # implement the recognizer
+    for index in range(test_set.num_items):
+
+        # For test_word get (X, lengths) => (array of feature lists, length of sequence within X)
+        test_word_X, test_word_lengths = test_set.get_item_Xlengths(index)
+        # initialize
+        best_likelihood = float("-inf")
+        best_match =  None
+        word_logLs = {}
+        
+        # Calculate LogL for test_word with model from models
+        # Assign that logL value as probability of match with corresponding word from models
+        for word, model in models.items():
+            try:
+                word_logLs[word] = model.score(test_word_X, test_word_lengths)
+            except:
+                # Assign -inf when model score could not be evaluated
+                word_logLs[word] = float("-inf")
+            # Update best/highest value of logLout of words processed from models and assing that word as best mathing word
+            if word_logLs[word] > best_likelihood:
+                best_likelihood = word_logLs[word]
+                best_match  = word
+                
+        #populate return values
+        probabilities.append(word_logLs)
+        guesses.append(best_match)
+
+    # return probabilities, guesses    
+    return probabilities, guesses
+    
+    
